c62f850c264c0bf847bda61d0b9c0e43d7a0d00b
Katrina Rosales <katrinarosales@icloud.com>
2023-08-03 23:08:50 +0200

    Add employee service for profile component

---
+++ src/interfaces/employee.tsx
@@ -0,0 +1,11 @@
+export class Employee {
+    private img: string;
+    private firstname: string;
+    private lastname: string;
+
+    constructor(img: string, firstname: string, lastname: string) {
+        this.img = img;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+}
---
+++ src/services/employees.tsx
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const baseURL = "https://api.1337co.de/v3/employees";
+
+// const getEmployees = async (): Promise<Employee[]> => {
+//     await axios.get(baseURL).then((response) => {
+//         return response.data
+//     })
+    
+// }
